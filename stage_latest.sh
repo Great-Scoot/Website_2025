2c73cedcb388df404a9d6ad2f32c2b90fff59cf5
uraspaz88 <scott.zehner7@gmail.com>
2024-11-27 16:46:53 -0500

    Testing WEBSITE_MODE=stage

--- stage_latest.sh
+++ stage_latest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+# NGINX: Enable maintenance mode
+# GUnicorn: Stop
 cd /app/Website_2025
 git pull origin master
+cp /app/Website_2025/nginx/docker.nginx.conf /etc/nginx/conf.d/
+nginx -t
 source venv/bin/activate
 pip install -r requirements.txt
 cd portfolio_backend
 python manage.py stage
-python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
-cd ../
-deactivate
-# sudo systemctl restart gunicorn
-# sudo systemctl restart nginx
+# start gunicorn
+# NGINX Disable maintenance mode
