14c49b301958f490ea2b5e8814faf29063078443
uraspaz88 <scott.zehner7@gmail.com>
2024-12-02 10:20:19 -0500

    Updates to Django Static stuff...

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -156,13 +156,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-django/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '..', 'portfolio_frontend', 'public')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-django')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
